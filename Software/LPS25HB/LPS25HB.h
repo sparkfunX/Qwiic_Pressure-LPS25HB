bd06bb4542c86a82ac65346a31ec45ef283ab515
Owen L - SFE <oclyke@gmail.com>
2018-05-30 15:17:25 -0600

    Created library files

---
+++ Software/LPS25HB/LPS25HB.h
@@ -0,0 +1,26 @@
+/*
+
+LPS25HB.h
+
+This is a basic interface to the LPS25HB I2C absolute digital output barometer. The IC can be found in
+the SparkFun Qwiic series of sensors
+
+Created: May 2018
+Last Updated: May 2018
+
+Authors:
+Owen Lyke
+
+*/
+
+#ifndef LPS25HB_H
+#define LPS25HB_H
+
+
+sdfsdfs
+
+
+
+
+
+#endif /* LPS25HB_H */
